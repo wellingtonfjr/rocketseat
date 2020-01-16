9dffdc3089b88a1d03a1c6b56e22c5eb90dbe69b
wellingtonfjr <wellington.fernandes@vamosparcelar.com.br>
2020-01-16 16:36:50 -0300

    Estrutra base

---
+++ modulo05/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ei José</h1>
+    </div>
+  );
+}
+
+export default App;
